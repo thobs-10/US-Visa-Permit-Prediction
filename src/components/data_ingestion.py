da83a348c3cdc5dbb67e427b37202cca6fd0c301
thobela <thobela.sixpence@gmail.com>
2024-12-03 21:48:38 +0200

    i extracted these functions from that one big function we had that was called cleaning data, to have these mini functions to abide by best practices

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -28,55 +28,38 @@ class DataIngestion:
         except Exception as e:
             raise AppException(e, sys)
     
-    def data_cleaning(self, df: pd.DataFrame) -> pd.DataFrame:
-        try:
-            logging.info("Performing data cleaning")
-            # Implement data cleaning logic here
-            # features with nan value
-            features_with_na=[features for features in df.columns if df[features].isnull().sum()>=1]
-            for feature in features_with_na:
-                if (np.round(df[feature].isnull().mean()*100,5)) > 25:
-                    df.drop(feature, axis=1, inplace=True)
-                    logging.info(f"Dropped {feature} due to high missing value percentage")
-                elif (np.round(df[feature].isnull().mean()*100,5)) > 50:
-                    df[feature].fillna(df[feature].median(), inplace=True)
-                    logging.info(f"Filled {feature} with median")
-            
-            # handling data types
-            logging.info("Correcting data types")
-            for column in df.columns:
-                if pd.api.types.is_object_dtype(df[column]):
-                    df[column] = df[column].astype(str)
-                elif pd.api.types.is_numeric_dtype(df[column]):
-                    df[column] = pd.to_numeric(df[column], errors='coerce')
-                elif pd.api.types.is_datetime64_dtype(df[column]):
-                    df[column] = pd.to_datetime(df[column], errors='coerce')
-                else:
-                    logging.warning(f"Unsupported data type for column: {column}")
-            # handling duplicates
-            logging.info("Handling duplicates")
-            df.drop_duplicates(inplace=True)
-            logging.info("Duplicates handled")
-            # remove outliers from the dataset
-            logging.info("Removing outliers")
-            # Assuming df is a DataFrame and 'numeric_column' is the column to detect outliers
-            # remove the outliers from each numeric column
-            numeric_columns = df.select_dtypes(include=['number']).columns
-            for column in numeric_columns:
-                # Calculate Q1, Q3, and IQR for the column
-                Q1 = df[column].quantile(0.25)
-                Q3 = df[column].quantile(0.75)
-                IQR = Q3 - Q1
-                
-                # Define outliers as values below Q1 - 1.5*IQR or above Q3 + 1.5*IQR
-                outliers = (df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR))
-                
-                # Remove outliers from the DataFrame
-                df = df[~outliers]
-            logging.info("Outliers removed")
-            return df
-        except Exception as e:
-            raise AppException(e, sys)
+    def handling_null_values(self, df: pd.DataFrame) -> pd.DataFrame:
+        logger.info("Performing data cleaning")
+        features_with_null_values=[features for features in df.columns if df[features].isnull().sum()>=1]
+        for feature in features_with_null_values:
+            if df[feature].dtype == 'categorical':
+                df[feature] = df[feature].fillna(df[feature].mode()[0], inplace=True)
+            else:
+                df[feature] = df[feature].fillna(df[feature].mean())
+        logger.debug("Handled missing values successfully")
+        return df
+        
+    def handling_data_type(self, df: pd.DataFrame) -> pd.DataFrame:
+        logger.info("Correcting data types")
+        for column in df.columns:
+            if pd.api.types.is_object_dtype(df[column]):
+                df[column] = df[column].astype(str)
+            elif pd.api.types.is_numeric_dtype(df[column]):
+                df[column] = pd.to_numeric(df[column], errors='coerce')
+            elif pd.api.types.is_datetime64_dtype(df[column]):
+                df[column] = pd.to_datetime(df[column], errors='coerce')
+            else:
+                logger.warning(f"Unsupported data type for column: {column}")
+        logger.debug("Data types corrected successfully")
+        return df
+    
+    def handling_duplicates(self, df: pd.DataFrame) -> pd.DataFrame:
+        logger.info("Handling duplicates")
+        duplicated = df[df.duplicated(keep=False)]
+        if duplicated.shape[0] > 0:
+            df = df.drop_duplicates(inplace= True)
+        logger.debug("Duplicates handled")
+        return df
     
     def save_data(self, df: pd.DataFrame) -> None:
         try:
