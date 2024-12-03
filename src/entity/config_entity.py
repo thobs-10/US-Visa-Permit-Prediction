b90099a28df1b6e44d32acc4978eee35b3621da9
thobela <thobela.sixpence@gmail.com>
2024-12-03 21:51:10 +0200

    refactored the config to reduce dificulty when calling the data classes and passing them via dependency injection, the goal here is to simplify the configs

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -1,58 +1,17 @@
 import os
 from dataclasses import dataclass
 from datetime import datetime
+from dotenv import load_dotenv
+
+root_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+load_dotenv(dotenv_path=os.path.join(root_dir, '.env'))
 
 
-@dataclass
-class DataPipelineConfig:
-    artifacts_dir: str
-
-@dataclass 
-class FeatureEngineeringPipelineConfig:
-    feature_engineering_dir: str
-
-@dataclass
-class ModelTrainingPipelineConfig:
-    model_training_dir: str
-
-@dataclass
+dataclass
 class DataIngestionConfig:
-    raw_folder: str
-    processed_folder: str
-
-    def __init__(self, data_pipeline_config: DataPipelineConfig):
-        self.raw_folder = os.path.join(
-            data_pipeline_config.artifacts_dir, "raw_data"
-        )
-        self.processed_folder = os.path.join(
-            data_pipeline_config.artifacts_dir, "processed_data"
-        )
-
+    raw_data_path: str = os.getenv('RAW_DATA_FILE')
 
 @dataclass
-class FeatureEngineeringConfig:
-    feature_engineering_dir: str
-    transformed_data_dir: str
-    transformed_features_file: str
-    transformed_target_file: str
+class DataPreprocessingConfig:
+    processed_data_path: str = os.getenv('PROCESSED_PATH_FILE')
 
-    def __init__(self, feature_engineering_pipeline_config: FeatureEngineeringPipelineConfig):
-        self.feature_engineering_dir = os.path.join(
-            feature_engineering_pipeline_config.feature_engineering_dir, "feature_engineered_data"
-        )
-        self.transformed_data_dir = os.path.join(self.feature_engineering_dir, "transformed_data")
-        self.transformed_features_file = os.path.join(self.transformed_data_dir, "features.parquet")
-        self.transformed_target_file = os.path.join(self.transformed_data_dir, "target.parquet")
-
-@dataclass
-class ModelTrainingConfig:
-    feature_engineered_data_dir: str
-    model_output_dir: str
-    model_file: str
-
-    def __init__(self, model_training_pipeline_config: ModelTrainingPipelineConfig, feature_engineering_config: FeatureEngineeringConfig):
-        self.feature_engineered_data_dir = feature_engineering_config.feature_engineering_dir
-        self.model_output_dir = os.path.join(
-            model_training_pipeline_config.model_training_dir, "model_output"
-        )
-        self.model_file = os.path.join(self.model_output_dir, "trained_model.pkl")
