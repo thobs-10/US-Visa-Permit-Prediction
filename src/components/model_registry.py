af33feb3ee61ab931548216edc5a3122f1f98efb
thobela <thobela.sixpence@gmail.com>
2025-02-23 09:26:35 +0200

    refactored version of the model registry, not yet tested.

--- src/components/model_registry.py
+++ src/components/model_registry.py
@@ -1,20 +1,19 @@
+import sys
+from loguru import logger
+
+from src.Exception import AppException
+from src.utils.main_utils import get_matching_experiments
 
 
-def register_model(self, experiment_key):
-        try:
-            logging.info("Starting the model registration phase")
-            api = API(api_key=os.getenv('API_KEY'))
-            experiment = api.get_experiment_by_key(experiment_key)
-            accuracy_condition = Metric("train_Accuracy") > 0.70
-            matching_experiments = api.query(
-                experiment.workspace, experiment.project_name, accuracy_condition)
-            if matching_experiments:
-                best_experiment = matching_experiments[0]
-                model_name = best_experiment.get_model_names()[0]
-                assets = best_experiment.get_model_asset_list(model_name=model_name)
-                feedback = best_experiment.register_model(model_name=model_name)
-                if feedback:
-                    logging.info("Successfully registered the model")
-        except Exception as e:
-            raise AppException(e, sys)
-
+def register_model(experiment_key: str):
+    try:
+        logger.info("Starting the model registration phase")
+        matching_experiments = get_matching_experiments()
+        if matching_experiments:
+            best_experiment = matching_experiments[0]
+            model_name = best_experiment.get_model_names()[0]
+            feedback = best_experiment.register_model(model_name=model_name)
+            if feedback:
+                logger.info("Successfully registered the model")
+    except Exception as e:
+        raise AppException(e, sys)
