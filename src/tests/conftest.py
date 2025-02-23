6f0965a13d500ff753dc791fe2b1fa15506b61f8
thobela <thobela.sixpence@gmail.com>
2025-02-23 09:26:03 +0200

    added these tests scripts so that later i can write test for their associated functional scripts

---
+++ src/tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+import pandas as pd
+
+
+@pytest.fixture
+def mock_raw_data_path(monkeypatch):
+    # Mock the environment variable for raw data path
+    monkeypatch.setenv("PROCESSED_DATA_FILE", "data/raw_data/EasyVisa.csv")
+
+
+@pytest.fixture
+def sample_data():
+    # Sample DataFrame to be returned by pd.read_csv
+    return pd.DataFrame(
+        {
+            "col1": [1, 2, 3],
+            "col2": ["A", "B", "C"],
+        }
+    )
