74033816fc52e17ba833be51911bb3c6370883ae
thobela <thobela.sixpence@gmail.com>
2025-03-06 22:20:17 +0200

    refactored training pipeline with its associated tests suite for testing edge cases.

--- src/components/model_training.py
+++ src/components/model_training.py
@@ -17,105 +17,117 @@ from src.utils.main_utils import (
     logging_metrics,
     feature_scaling,
 )
+from zenml import step
+from feast import FeatureStore
 
 
 load_dotenv()
 
 
-class ModelTraining(Component):
-    def __init__(
-        self,
-        model_training_config: ModelTrainingConfig = ModelTrainingConfig(),
-        feature_engineering_config: FeatureEngineeringConfig = FeatureEngineeringConfig(),
-    ):
-        self.model_training_config = model_training_config
-        self.feature_engineering_config = feature_engineering_config
-
-    def load_data(self) -> tuple:
-        try:
-            logger.info(
-                "Loading feature engineered data(features.parquet and target.csv)"
+@step(enable_cache=False)
+def load_data() -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """Load the feature engineered data from the feast feature store"""
+    try:
+        logger.info("Loading feature engineered data from feature store.")
+        features_df = pd.read_parquet(
+            os.path.join(
+                FeatureEngineeringConfig.feature_engineering_dir, "features.parquet"
             )
-            transformed_features_file_path = os.path.join(
-                self.feature_engineering_config.feature_engineering_dir
-            )
-            X_df = pd.read_parquet(
-                os.path.join(transformed_features_file_path, "features.parquet")
-            )
-            y_df = pd.read_csv(
-                os.path.join(transformed_features_file_path, "target.csv")
-            )
-            logger.info(
-                "Processed data loaded and converted to NumPy arrays successfully"
-            )
-            return X_df, y_df
-        except Exception as e:
-            raise e
-
-    def split_data(self, X, y) -> tuple:
-        logger.info("Splitting data into training, validation, and test sets")
-        X_train, X_temp, y_train, y_temp = train_test_split(
-            X, y, train_size=0.7, random_state=42
         )
-        X_valid, X_test, y_valid, y_test = train_test_split(
-            X_temp, y_temp, test_size=1 / 3, random_state=42
+        target_df = pd.read_csv(
+            os.path.join(FeatureEngineeringConfig.feature_engineering_dir, "target.csv")
         )
-        logger.info("Data split completed successfully")
-        return X_train, y_train, X_test, y_test, X_valid, y_valid
-
-    def train_model(
-        self,
-        X_train: pd.DataFrame,
-        X_valid: pd.DataFrame,
-        y_train: np.ndarray,
-        y_valid: np.ndarray,
-    ) -> Tuple[str, ColumnTransformer]:
-        models = get_models()
-        logger.info("Training model phase")
-        experiment = tracking_details_init()
-        for model_name, model in models.items():
-            experiment.add_tag(f"Model: {model_name}")
-            with experiment.train():
-                logger.info(f"Training {model_name}")
-                column_transformer, X_train_scaled, X_valid_scaled = feature_scaling(
-                    X_train,
-                    X_valid,
-                )
-                y_pred, model_path = train_and_save(
-                    X_train_scaled, X_valid_scaled, y_train, model_name, model
-                )
-                experiment.log_model(f"{model_name}", model_path)
-                logging_metrics(experiment, y_pred, y_valid)
-
-        exp_key = experiment.get_key()
-        logger.info("models trained and evaluated successfully")
-        return exp_key, column_transformer
-
-    def save_preprocessor(
-        self, preprocessor: ColumnTransformer, filename: str = "preprocessor.joblib"
-    ) -> None:
-        try:
-            logger.info("Saving preprocessor..")
-            path = os.environ["ARTIFACTS_PATH"]
-            os.makedirs(path, exist_ok=True)
-            output_path = os.path.join(path, filename)
-            joblib.dump(preprocessor, output_path)
-        except Exception as e:
-            raise e
-
-    def save_training_data(
-        self,
-        X_train: pd.DataFrame,
-        X_valid: pd.DataFrame,
-        y_train: pd.Series,
-        y_valid: pd.Series,
-        filename: str = "training_data",
-    ) -> None:
-        logger.info("Saving training data to parquet format")
-        X_train.to_parquet(
-            os.path.join(os.environ["DATA"], filename, "X_train.parquet")
+        fs = FeatureStore(
+            repo_path="/Users/thobelasixpence/Documents/mlops-zoomcamp-project-2024/US-Visa-Permit-Prediction/my_feature_store/feature_repo",
         )
-        X_valid.to_parquet(os.path.join(filename, "X_valid.parquet"))
-        y_train.to_csv(os.path.join(filename, "y_train.csv"))
-        y_valid.to_csv(os.path.join(filename, "y_valid.csv"))
-        logger.debug("Training data saved successfully")
+        fs.get_historical_features(
+            entity_df=features_df,
+            features=fs.get_feature_service("passenger_features"),
+        )
+        logger.info("Processed data loaded and converted to NumPy arrays successfully")
+        return features_df, target_df
+    except Exception as e:
+        raise e
+
+
+# @step
+def split_data(
+    X: pd.DataFrame, y: pd.DataFrame
+) -> Tuple[
+    pd.DataFrame,
+    pd.Series,
+    pd.DataFrame,
+    pd.Series,
+    pd.DataFrame,
+    pd.Series,
+]:
+    logger.info("Splitting data into training, validation, and test sets")
+    X.replace([np.inf, -np.inf], np.nan, inplace=True)
+    X.dropna(inplace=True)
+    y.replace([np.inf, -np.inf], np.nan, inplace=True)
+    y.dropna(inplace=True)
+
+    target = y.iloc[:, 1]
+    X_train, X_temp, y_train, y_temp = train_test_split(
+        X, target, train_size=0.7, random_state=42
+    )
+    X_valid, X_test, y_valid, y_test = train_test_split(
+        X_temp, y_temp, test_size=1 / 3, random_state=42
+    )
+    logger.info("Data split completed successfully")
+    return X_train, y_train, X_test, y_test, X_valid, y_valid
+
+
+def train_model(
+    X_train: pd.DataFrame,
+    X_valid: pd.DataFrame,
+    y_train: np.ndarray,
+    y_valid: np.ndarray,
+) -> Tuple[str, ColumnTransformer]:
+    models = get_models()
+    logger.info("Training model phase")
+    experiment = tracking_details_init()
+    for model_name, model in models.items():
+        experiment.add_tag(f"Model: {model_name}")
+        with experiment.train():
+            logger.info(f"Training {model_name}")
+            column_transformer, X_train_scaled, X_valid_scaled = feature_scaling(
+                X_train,
+                X_valid,
+            )
+            y_pred, model_path = train_and_save(
+                X_train_scaled, X_valid_scaled, y_train, model_name, model
+            )
+            experiment.log_model(f"{model_name}", model_path)
+            logging_metrics(experiment, y_pred, y_valid)
+
+    exp_key = experiment.get_key()
+    logger.info("models trained and evaluated successfully")
+    return exp_key, column_transformer
+
+
+def save_preprocessor(
+    preprocessor: ColumnTransformer, filename: str = "preprocessor.joblib"
+) -> None:
+    try:
+        logger.info("Saving preprocessor..")
+        os.makedirs(ModelTrainingConfig.model_artifact_dir, exist_ok=True)
+        output_path = os.path.join(ModelTrainingConfig.model_artifact_dir, filename)
+        joblib.dump(preprocessor, output_path)
+    except Exception as e:
+        raise e
+
+
+def save_training_data(
+    X_train: pd.DataFrame,
+    X_valid: pd.DataFrame,
+    y_train: pd.Series,
+    y_valid: pd.Series,
+    filename: str = "training_data",
+) -> None:
+    logger.info("Saving training data to parquet format")
+    X_train.to_parquet(os.path.join(os.environ["DATA"], filename, "X_train.parquet"))
+    X_valid.to_parquet(os.path.join(filename, "X_valid.parquet"))
+    y_train.to_csv(os.path.join(filename, "y_train.csv"))
+    y_valid.to_csv(os.path.join(filename, "y_valid.csv"))
+    logger.debug("Training data saved successfully")
---
+++ src/tests/test_model_training.py
@@ -0,0 +1,157 @@
+import pytest
+import os
+import pandas as pd
+from unittest.mock import patch, MagicMock
+import numpy as np
+from src.components.model_training import (
+    load_data,
+    split_data,
+    train_model,
+    save_preprocessor,
+)
+from src.entity.config_entity import ModelTrainingConfig
+from sklearn.compose import ColumnTransformer
+
+
+def test_split_data_success():
+    """Test successful splitting of data."""
+    X = pd.DataFrame(
+        {
+            "yr_of_estab": [2000, 2010, 2015, 2020, 2005, 1995, 2018, 2008, 2012, 2003],
+            "other_column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
+    )
+    y = pd.DataFrame(
+        {
+            "Unnamed": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            "series": [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        }
+    )
+    result = split_data(X, y)
+    assert len(result) == 6
+    assert isinstance(result[0], pd.DataFrame)
+    assert isinstance(result[1], pd.Series)
+    assert isinstance(result[2], pd.DataFrame)
+    assert isinstance(result[3], pd.Series)
+    assert isinstance(result[4], pd.DataFrame)
+    assert isinstance(result[5], pd.Series)
+
+
+def test_split_data_with_nan_values():
+    """Test splitting of data with NaN values."""
+    X = pd.DataFrame(
+        {
+            "yr_of_estab": [2000, 2010, 2015, 2020, 2005, 1995, 2018, 2008, 2012, 2003],
+            "other_column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
+    )
+    y = pd.DataFrame(
+        {
+            "Unnamed": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            "series": [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        }
+    )
+    X.iloc[0, 0] = np.nan
+    y.iloc[0, 0] = np.nan
+    result = split_data(X, y)
+    assert len(result) == 6
+    assert isinstance(result[0], pd.DataFrame)
+    assert isinstance(result[1], pd.Series)
+    assert isinstance(result[2], pd.DataFrame)
+    assert isinstance(result[3], pd.Series)
+    assert isinstance(result[4], pd.DataFrame)
+    assert isinstance(result[5], pd.Series)
+
+
+def test_split_data_with_infinite_values():
+    """Test splitting of data with infinite values."""
+    X = pd.DataFrame(
+        {
+            "yr_of_estab": [2000, 2010, 2015, 2020, 2005, 1995, 2018, 2008, 2012, 2003],
+            "other_column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
+    )
+    y = pd.DataFrame(
+        {
+            "Unnamed": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            "series": [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+        }
+    )
+    X.iloc[0, 0] = np.inf
+    y.iloc[0, 0] = np.inf
+    result = split_data(X, y)
+    assert len(result) == 6
+    assert isinstance(result[0], pd.DataFrame)
+    assert isinstance(result[1], pd.Series)
+    assert isinstance(result[2], pd.DataFrame)
+    assert isinstance(result[3], pd.Series)
+    assert isinstance(result[4], pd.DataFrame)
+    assert isinstance(result[5], pd.Series)
+
+
+def test_train_model_success():
+    """Test successful training of model."""
+    X_train = pd.DataFrame(
+        {
+            "yr_of_estab": [2000, 2010, 2015, 2020, 2005, 1995, 2018, 2008, 2012, 2003],
+            "other_column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
+    )
+    X_valid = pd.DataFrame(
+        {
+            "yr_of_estab": [2000, 2010, 2015, 2020, 2005, 1995, 2018, 2008, 2012, 2003],
+            "other_column": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        }
+    )
+    y_train = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+    y_valid = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+    mock_column_transformer = MagicMock(spec=ColumnTransformer)
+    mock_X_train_scaled = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+    mock_X_valid_scaled = np.array([[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]])
+    mock_y_pred = np.array([1, 0, 1, 0, 1, 0, 1, 0, 1, 0])
+    mock_model_path = "model.pkl"  # this can be a string that is formed from by the comet_ml library which cab anything
+    with patch(
+        "src.components.model_training.feature_scaling",
+        return_value=(
+            mock_column_transformer,
+            mock_X_train_scaled,
+            mock_X_valid_scaled,
+        ),
+    ):
+        with patch(
+            "src.components.model_training.train_and_save",
+            return_value=(mock_y_pred, mock_model_path),
+        ):
+            result = train_model(X_train, X_valid, y_train, y_valid)
+            assert len(result) == 2
+            assert isinstance(result[0], str)
+            assert isinstance(result[1], ColumnTransformer)
+            # assert result[0] == mock_model_path
+            assert result[1] == mock_column_transformer
+
+
+def test_save_preprocessor_successfully():
+    """Test successful saving of preprocessor."""
+    mock_column_transformer = MagicMock(spec=ColumnTransformer)
+    model_artifact_dir = "src/models/artifacts/"
+    with patch("joblib.dump") as mock_joblib_dump:
+        with patch(
+            "src.entity.config_entity.ModelTrainingConfig.model_artifact_dir",
+            model_artifact_dir,
+        ):
+            output_path = os.path.join(model_artifact_dir, "preprocessor.joblib")
+            results = save_preprocessor(mock_column_transformer)
+            mock_joblib_dump.assert_called_once_with(
+                mock_column_transformer, output_path
+            )
+            assert results is None
+
+
+def test_save_preprocessor_with_exception():
+    """Test saving of preprocessor with an exception."""
+    mock_column_transformer = MagicMock(spec=ColumnTransformer)
+    with patch("joblib.dump") as mock_joblib_dump:
+        mock_joblib_dump.side_effect = Exception("An error occurred")
+        with pytest.raises(Exception):
+            save_preprocessor(mock_column_transformer)
+            mock_joblib_dump.assert_called_once()
