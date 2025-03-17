b1dede0b5d8ac3681094ffb5fdeb12e09d4af8a3
thobela <thobela.sixpence@gmail.com>
2025-03-17 22:47:11 +0200

    refactored model registry and changed to mlflow

--- src/components/model_registry.py
+++ src/components/model_registry.py
@@ -1,19 +1,35 @@
-import sys
 from loguru import logger
+from sklearn.pipeline import Pipeline
+import mlflow
+from mlflow.entities import ViewType
+from mlflow.client import MlflowClient
 
-from src.Exception import AppException
-from src.utils.main_utils import get_matching_experiments
+tracking_uri = mlflow.get_tracking_uri()
+client = MlflowClient(tracking_uri=tracking_uri)
 
 
-def register_model(experiment_key: str):
-    try:
-        logger.info("Starting the model registration phase")
-        matching_experiments = get_matching_experiments()
-        if matching_experiments:
-            best_experiment = matching_experiments[0]
-            model_name = best_experiment.get_model_names()[0]
-            feedback = best_experiment.register_model(model_name=model_name)
-            if feedback:
-                logger.info("Successfully registered the model")
-    except Exception as e:
-        raise AppException(e, sys)
+def register_model(model_pipeline: Pipeline) -> None:
+    """
+    Registers the best model from the hyperparameter tuning experiment in MLflow.
+
+    Parameters:
+    best_model (BaseEstimator): The best model to be registered.
+
+    Raises:
+    ValueError: If the hyperparameter tuning experiment does not exist.
+    """
+    tuning_experiment = client.get_experiment_by_name("Hyperparameter Tuning Phase")
+    if not tuning_experiment:
+        raise ValueError("Hyperparameter tuning experiment does not exist")
+    top_n = 1  # Define the variable top_n
+    runs = client.search_runs(
+        experiment_ids=tuning_experiment.experiment_id,
+        run_view_type=ViewType.ACTIVE_ONLY,
+        max_results=top_n,
+        order_by=["metrics.Accuracy ASC"],
+    )
+    best_run = runs[0]
+    model_uri = f"runs:/{best_run.info.run_id}/model"
+    model_name = model_pipeline.__class__.__name__
+    mlflow.register_model(model_uri=model_uri, name=model_name)
+    logger.info(f"Model '{model_name}' registered successfully")
