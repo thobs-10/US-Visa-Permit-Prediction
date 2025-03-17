8f09880b32e708d00bd950bc3d95cefc37cb27c2
thobela <thobela.sixpence@gmail.com>
2025-03-17 22:51:28 +0200

    refactored the pipeline to use functional design instead of OOP for ease of things and design

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,33 +1,43 @@
-import os
-import sys
+from loguru import logger
+from src.components.model_training import (
+    load_data,
+    split_data,
+    train_model,
+    save_preprocessor,
+    save_training_data,
+)
+from src.components.model_tuning import hyperparameter_tuning
+from src.components.model_validation import model_evaluation, save_model_pipeline
+from src.components.model_registry import register_model
 
-from src.logger import logging
-from src.Exception import AppException
+from zenml import pipeline
 
-from src.components.model_training import ModelTraining
 
-from src.entity.artifact_entity import ModelTrainingArtifact
-from src.entity.config_entity import ModelTrainingConfig, ModelTrainingPipelineConfig
+def run_model_training() -> None:
+    try:
+        logger.info("Starting model training pipeline")
+        X, y = load_data()
+        X_train, y_train, X_test, y_test, X_valid, y_valid = split_data(X, y)
+        column_transformer, chosen_model_path, best_model_name = train_model(
+            X_train, X_valid, y_train, y_valid
+        )
+        save_preprocessor(column_transformer)
+        save_training_data(X_train, X_valid, y_train, y_valid)
+        best_model = hyperparameter_tuning(
+            X_train,
+            X_valid,
+            y_train,
+            y_valid,
+            chosen_model_path,
+            best_model_name,
+            column_transformer,
+        )
+        model = model_evaluation(X_test, y_test, X_valid, best_model, column_transformer)
+        save_model_pipeline(model)
+        register_model(model)
+    except Exception as e:
+        raise e
 
-from src.entity.config_entity import FeatureEngineeringConfig
 
-class ModelTrainingPipeline:
-    def __init__(self, config: ModelTrainingConfig, artifact : ModelTrainingArtifact, featureEngineeringConfig: FeatureEngineeringConfig):
-        self.config = config
-        self.artifact = artifact
-        self.feature_engineering_config = featureEngineeringConfig
-        # self.model_training = ModelTraining(self.artifact)
-    
-    def run_model_training(self) -> ModelTrainingArtifact:
-        try:
-            logging.info("Starting model training pipeline")
-            model_training = ModelTraining(self.config, self.artifact)
-            X, y = model_training.load_data(self.feature_engineering_config)
-            X_train, X_valid, X_test, y_train, y_valid, y_test = model_training.split_data(X, y)
-            exp_key = model_training.train_model(X_train, X_valid, y_train, y_valid)
-            hyperparameter_exp_key = model_training.hyperparameter_tuning(exp_key, X_train, y_train, X_valid, y_valid)
-            model_training.evaluate_model(hyperparameter_exp_key, X_test, y_test)
-            model_training.register_model(hyperparameter_exp_key)
-            # model_training.model_registration()
-        except Exception as e:
-            raise AppException(e, sys)
+if __name__ == "__main__":
+    run_model_training()
