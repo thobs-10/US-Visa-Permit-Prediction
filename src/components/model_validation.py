674dc35d6a753ea3308a275146db1f501a8acc20
thobela <thobela.sixpence@gmail.com>
2025-03-17 22:50:11 +0200

    refactored the code to make sure we do proper validation by checking the metrics of interest before passing the model to model registry

--- src/components/model_validation.py
+++ src/components/model_validation.py
@@ -1,13 +1,10 @@
 import os
 import pandas as pd
-import numpy as np
 from loguru import logger
 import joblib
-from typing import Optional, Union
+from typing import Union
 
-from comet_ml import Experiment
-from sklearn.base import BaseEstimator
-from sklearn.pipeline import make_pipeline
+from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import (
     RandomForestClassifier,
@@ -18,14 +15,13 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
-from src.utils.main_utils import feature_scaling
 from src.entity.config_entity import ModelTuningConfig
 from src.utils.main_utils import log_metrics_terminal
 
 
-def evaluate_model(
+def model_evaluation(
     X_test: pd.DataFrame,
-    y_test: np.ndarray,
+    y_test: pd.Series,
     X_val: pd.DataFrame,
     model: Union[
         LogisticRegression,
@@ -36,31 +32,29 @@ def evaluate_model(
         SVC,
         AdaBoostClassifier,
     ],
-    experiment: Experiment,
-    exp_key: Optional[str] = None,
-) -> BaseEstimator:
-    logger.info(" Starting the evaluation phase")
-    column_transformer, X_valid_scaled, X_test_scaled = feature_scaling(
-        X_val,
-        X_test,
-    )
-    y_pred = model.predict(X_test_scaled)
-    log_metrics_terminal(y_pred, y_test)
+    column_transformer: ColumnTransformer,
+    threshold: float = 0.50,
+) -> Pipeline:
+    logger.info(" Starting the model evaluation phase")
+    model_pipeline = make_pipeline(column_transformer, model)
+    logger.info("Model prediction phase")
+    y_pred = model_pipeline.predict(X_test)
+    y_test_numpy = y_test.to_numpy()
+    metrics_dict = log_metrics_terminal(y_pred, y_test_numpy)  # type: ignore
+    if metrics_dict["Accuracy"] and metrics_dict["F1 Score"] < threshold:
+        logger.error("Model evaluation failed. F1 score is less than threshold.")
+        raise ValueError("Model evaluation failed, accuracy is less than threshold.")
     logger.info("Completed model evaluation.")
+    return model_pipeline
 
-    return model
 
-
-def save_model_pipeline(
-    model: BaseEstimator, column_transformer: ColumnTransformer
-) -> None:
+def save_model_pipeline(model_pipeline: Pipeline) -> None:
     try:
-        model_pipeline = make_pipeline(column_transformer, model)
         model_path = os.path.join(
             ModelTuningConfig.model_artifact_dir, " model_pipeline"
         )
         os.makedirs(model_path, exist_ok=True)
+        joblib.dump(model_pipeline, os.path.join(model_path, "model_pipeline.pkl"))
         logger.info(f"Saving model pipeline to {model_path}")
-        joblib.dump(model_pipeline, model_path)
     except Exception as e:
         raise e
