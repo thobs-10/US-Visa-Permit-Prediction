a878abe12acc99849b70d4effc313ae8f95c2727
thobela <thobela.sixpence@gmail.com>
2025-03-30 10:41:05 +0200

    update: made changes to the model validation script because of code qa tools

--- src/components/model_validation.py
+++ src/components/model_validation.py
@@ -1,39 +1,31 @@
 import os
+from datetime import datetime
+
+import joblib
 import pandas as pd
 from loguru import logger
-import joblib
-from typing import Union
-
-from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.ensemble import (
-    RandomForestClassifier,
     AdaBoostClassifier,
     GradientBoostingClassifier,
+    RandomForestClassifier,
 )
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.pipeline import Pipeline, make_pipeline
 from sklearn.svm import SVC
+from sklearn.tree import DecisionTreeClassifier
+from zenml import step
+
 from src.entity.config_entity import ModelTuningConfig
 from src.utils.main_utils import log_metrics_terminal
-from zenml import step
 
 
 @step
 def model_evaluation(
     X_test: pd.DataFrame,
     y_test: pd.Series,
-    X_val: pd.DataFrame,
-    model: Union[
-        LogisticRegression,
-        RandomForestClassifier,
-        DecisionTreeClassifier,
-        GradientBoostingClassifier,
-        KNeighborsClassifier,
-        SVC,
-        AdaBoostClassifier,
-    ],
+    model: LogisticRegression | RandomForestClassifier | DecisionTreeClassifier | GradientBoostingClassifier | KNeighborsClassifier | SVC | AdaBoostClassifier,
     column_transformer: ColumnTransformer,
     threshold: float = 0.50,
 ) -> Pipeline:
@@ -53,9 +45,10 @@ def model_evaluation(
 @step
 def save_model_pipeline(model_pipeline: Pipeline) -> None:
     try:
+        saving_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         model_path = os.path.join(ModelTuningConfig.model_artifact_dir, " model_pipeline")
         os.makedirs(model_path, exist_ok=True)
-        joblib.dump(model_pipeline, os.path.join(model_path, "model_pipeline.pkl"))
+        joblib.dump(model_pipeline, os.path.join(model_path, f"model_pipeline_{saving_timestamp}.pkl"))
         logger.info(f"Saving model pipeline to {model_path}")
     except Exception as e:
         raise e
