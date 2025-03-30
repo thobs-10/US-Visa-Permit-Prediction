a2bd004de19492556791671105276403df20ea6c
thobela <thobela.sixpence@gmail.com>
2025-03-30 10:19:15 +0200

    update: made changes forced by the code qa tools for feaure engineering script

--- src/components/feature_engineering.py
+++ src/components/feature_engineering.py
@@ -1,31 +1,26 @@
 import os
+from datetime import date, datetime
+
 import pandas as pd
-from datetime import date
-
-
-from typing import Tuple
-from datetime import datetime
-
+from feast import (
+    Entity,
+    FeatureService,
+    FeatureStore,
+    FeatureView,
+    FileSource,
+    ValueType,
+)
 from loguru import logger
-from src.entity.config_entity import FeatureEngineeringConfig
-from src.entity.config_entity import DataPreprocessingConfig
+from zenml import step
+
+from src.entity.config_entity import DataPreprocessingConfig, FeatureEngineeringConfig
 from src.utils.main_utils import (
-    get_skewed_features,
-    separate_data,
     apply_power_transform,
     encode_target,
     get_latest_modified_file,
+    get_skewed_features,
     get_statistical_properties,
-)
-from zenml import step
-
-from feast import (
-    FeatureStore,
-    FeatureView,
-    Entity,
-    ValueType,
-    FileSource,
-    FeatureService,
+    separate_data,
 )
 
 
@@ -37,11 +32,7 @@ def load_data() -> pd.DataFrame:
         if not os.path.exists(processed_folder):
             logger.warning("Cannot find processed folder")
             return pd.DataFrame()
-        files = [
-            f
-            for f in os.listdir(processed_folder)
-            if os.path.isfile(os.path.join(processed_folder, f))
-        ]
+        files = [f for f in os.listdir(processed_folder) if os.path.isfile(os.path.join(processed_folder, f))]
         processed_files = [f for f in files if f.startswith("processed_data_")]
         if not processed_files:
             logger.warning("No processed data files found in the directory.")
@@ -71,8 +62,7 @@ def feature_extraction(df: pd.DataFrame) -> pd.DataFrame:
     try:
         df["yr_of_estab"] = pd.to_numeric(df["yr_of_estab"], errors="raise")
     except ValueError as e:
-        logger.error(f"Invalid data in 'yr_of_estab' column: {e}")
-        raise TypeError("The 'yr_of_estab' column contains non-numeric data.")
+        logger.exception(f"Invalid data in 'yr_of_estab' column: {e}")
 
     df["company_age"] = current_year - df["yr_of_estab"]
     df.drop("yr_of_estab", inplace=True, axis=1)
@@ -87,9 +77,7 @@ def removing_outliers(df: pd.DataFrame) -> pd.DataFrame:
     outlier_mask = pd.Series(data=False, index=df.index)
     for column in numeric_columns:
         Q1, Q3, IQR = get_statistical_properties(column, df)
-        column_outliers = (df[column] < (Q1 - 1.5 * IQR)) | (
-            df[column] > (Q3 + 1.5 * IQR)
-        )
+        column_outliers = (df[column] < (Q1 - 1.5 * IQR)) | (df[column] > (Q3 + 1.5 * IQR))
         outlier_mask = outlier_mask | column_outliers
     df = df[~outlier_mask]
     logger.info("Outliers removed")
@@ -99,11 +87,9 @@ def removing_outliers(df: pd.DataFrame) -> pd.DataFrame:
 @step
 def feature_transformations(
     df: pd.DataFrame,
-) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
+) -> tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
     logger.info("Performing feature transformations")
-    continuous_features = df.select_dtypes(
-        include=["float64", "int64"]
-    ).columns.tolist()
+    continuous_features = df.select_dtypes(include=["float64", "int64"]).columns.tolist()
     transform_features = get_skewed_features(df, continuous_features)
     if len(transform_features) > 0:
         logger.info(f"Features to be transformed: {transform_features}")
@@ -112,11 +98,11 @@ def feature_transformations(
         X = apply_power_transform(df, X, transform_features)
         logger.info("Feature transformations completed successfully")
         return df, X, y
-    else:
-        logger.info("No features to be transformed")
-        X, y = separate_data(df)
-        y = encode_target(y)
-        return df, X, y
+
+    logger.info("No features to be transformed")
+    X, y = separate_data(df)
+    y = encode_target(y)
+    return df, X, y
 
 
 @step
@@ -129,13 +115,15 @@ def save(X: pd.DataFrame, y: pd.Series, df: pd.DataFrame) -> None:
             os.makedirs(fullpath, exist_ok=True)
         else:
             logger.info(f"Directory already exists: {fullpath}")
-        feature_filename = os.path.join(fullpath, "features.parquet")
-        target_filename = os.path.join(fullpath, "target.csv")
+
+        saving_timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        feature_filename = os.path.join(fullpath, f"features_{saving_timestamp}.parquet")
+        target_filename = os.path.join(fullpath, f"target_{saving_timestamp}.csv")
         X["event_timestamp"] = datetime.now()
         X.to_parquet(feature_filename)
         y.to_csv(target_filename)
         df["event_timestamp"] = datetime.now()
-        df.to_parquet(os.path.join(fullpath, "full_data.parquet"))
+        df.to_parquet(os.path.join(fullpath, f"full_data_{saving_timestamp}.parquet"))
         logger.info("Processed data saved successfully")
     except Exception as e:
         raise e
