d7f8e2525f51589fd977d53a443f1ac7a57f2353
thobela <thobela.sixpence@gmail.com>
2025-04-05 22:01:20 +0200

    new: added a bash script for enabling task runner for the system and running the whole system with one command for local reproducibility.

---
+++ run.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# Function to install the package
+install_package() {
+  echo "Installing the package..."
+  pip install .
+}
+
+setup_zenml() {
+  echo "Setting up ZenML..."
+  export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
+  zenml login --local
+}
+
+# Function to start Feast UI
+start_feast() {
+  echo "Starting Feast UI..."
+  cd my_feature_store/feature_repo
+  feast ui
+  cd ../.. # Return to the root directory
+}
+
+# Function to start MLflow server
+start_mlflow() {
+  echo "Starting MLflow server..."
+  mlflow server --host 127.0.0.1 --port 8085
+}
+
+# Function to run the pipelines
+run_pipelines() {
+  echo "Running data ingestion, feature engineering, and model training pipelines..."
+  python run_pipelines.py
+}
+
+# Function to run pre-commit checks
+run_pre_commit() {
+  echo "Running pre-commit checks..."
+  SKIP=no-commit-to-branch pre-commit run --all-files
+}
+
+# Main execution
+echo "Starting the development process..."
+
+# Install the package
+install_package
+
+# Start feast ui and mlflow in the background
+setup_zenml &
+start_feast &
+start_mlflow &
+
+# Wait for feast and mlflow to finish starting up.
+sleep 5
+
+# Run the pipelines
+run_pipelines
+
+# Run pre-commit checks
+run_pre_commit
+
+echo "Development process completed."
