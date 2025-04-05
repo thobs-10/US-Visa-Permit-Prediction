13532f8ea41f6eb61eeb54cf223c9f1f7cb2caa6
thobela <thobela.sixpence@gmail.com>
2025-04-05 15:06:33 +0200

    update: new suggestion by the pre-commit configs

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,16 +1,16 @@
 from loguru import logger
+from zenml import pipeline
+
+from src.components.model_registry import register_model
 from src.components.model_training import (
     load_data,
-    split_data,
-    train_model,
     save_preprocessor,
     save_training_data,
+    split_data,
+    train_model,
 )
 from src.components.model_tuning import hyperparameter_tuning
 from src.components.model_validation import model_evaluation, save_model_pipeline
-from src.components.model_registry import register_model
-
-from zenml import pipeline
 
 
 @pipeline
@@ -19,9 +19,7 @@ def run_model_training() -> None:
         logger.info("Starting model training pipeline")
         X, y = load_data()
         X_train, y_train, X_test, y_test, X_valid, y_valid = split_data(X, y)
-        column_transformer, chosen_model_path, best_model_name = train_model(
-            X_train, X_valid, y_train, y_valid
-        )
+        column_transformer, chosen_model_path, best_model_name = train_model(X_train, X_valid, y_train, y_valid)
         save_preprocessor(column_transformer)
         save_training_data(X_train, X_valid, y_train, y_valid)
         best_model = hyperparameter_tuning(
@@ -33,7 +31,7 @@ def run_model_training() -> None:
             best_model_name,
             column_transformer,
         )
-        model = model_evaluation(X_test, y_test, X_valid, best_model, column_transformer)
+        model = model_evaluation(X_test, y_test, best_model, column_transformer)
         save_model_pipeline(model)
         register_model(model)
     except Exception as e:
